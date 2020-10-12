0c09bb7dc7e6a2aba89a479bc65f14c5fddd7586
jad803 <jad803@msstate.edu>
2020-10-11 20:01:38 -0500

    Web page will load now. Go to /login and it will bring to a sample login page. Needs a bunch of things added, but a sample page now exists.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+Route::get('/hello', function () {
+    return '<h1>Hello World<h1>';
+});
+*/
+
+Route::get('/about', function() {
+    return view('pages.about');
+});
+
+Route::get('/login', function() {
+    return view('pages.login_page');
+});

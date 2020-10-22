f01d10277872291f7f6b8f5c4bc9dd4dc833e064
omar <theomarq@gmail.com>
2020-10-22 16:33:16 -0500

    database seeding

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -22,18 +23,20 @@ class UserFactory extends Factory
      */
     public function definition()
     {
-        $name = $this->faker->name;
-        $name = explode(" ", $name);
+        $first_name = $this->faker->firstName;
+        $last_name = $this->faker->lastName;
 
-        $first_initial = substr($name[0], 0, 1);
-        $last_initial = substr($name[1], 0, 1);
+        $first_initial = substr($first_name[0], 0, 1);
+        $last_initial = substr($last_name[1], 0, 1);
+        $netID = strtolower($first_initial) . strtolower($last_initial) . rand(1, 400);
 
         return [
-            'first_name' => $name[0],
-            'last_name' => $name[1],
-            'email' => $this->faker->unique()->safeEmail,
+            'first_name' => $first_name,
+            'last_name' => $last_name,
+            'email' => $netID."@msstate.edu",
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'major' => "CS",
+            'password' => $this->faker->lexify("??????????"), // password
             'remember_token' => Str::random(10),
         ];
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
@@ -16,15 +17,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        /*User::factory()
+        User::factory()
             ->times(20)
-            ->create();*/
-            DB::table('users')->insert([
+            ->create();
+            /*DB::table('users')->insert([
                 'first_name' => Str::random(10),
                 'last_name' => Str::random(10),
                 'email' => Str::random(10).'@msstate.edu',
                 'major' => Str::random(10),
                 'password' => Hash::make('password'),
-            ]);
+            ]);*/
     }
 }

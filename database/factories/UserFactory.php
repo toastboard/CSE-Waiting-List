7e27dd3028dc98d9afd714f30e188d2acdb07a54
omar <theomarq@gmail.com>
2020-10-22 15:56:11 -0500

    hi

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,8 +22,15 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $name = $this->faker->name;
+        $name = explode(" ", $name);
+
+        $first_initial = substr($name[0], 0, 1);
+        $last_initial = substr($name[1], 0, 1);
+
         return [
-            'name' => $this->faker->name,
+            'first_name' => $name[0],
+            'last_name' => $name[1],
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(10)->create();
+        /*User::factory()
+            ->times(20)
+            ->create();*/
+            DB::table('users')->insert([
+                'first_name' => Str::random(10),
+                'last_name' => Str::random(10),
+                'email' => Str::random(10).'@msstate.edu',
+                'major' => Str::random(10),
+                'password' => Hash::make('password'),
+            ]);
     }
 }

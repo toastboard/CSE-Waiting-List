f4be812cef480bfefe01b561ce7d1fd8d2a07456
CJ Nguyen <cjnguyen.99@gmail.com>
2020-10-23 12:37:31 -0500

    Organized forms and removed netID form (will need to pull from DB

--- resources/views/pages/form.blade.php
+++ resources/views/pages/form.blade.php
@@ -7,20 +7,21 @@
     <div class='form_group'>
         {{Form::label('first_name', 'First Name')}}
         {{Form::text('first_name', '', ['class' => 'form-control'])}}
-        
+    </div>
+    <div class='form_group'>
         {{Form::label('last_name', 'Last Name')}}
         {{Form::text('last_name', '', ['class' => 'form-control'])}}
-
-        {{Form::label('student_ID', 'Last Name')}}
+    </div>
+    <div class='form_group'>
+        {{Form::label('student_ID', 'MSU ID')}}
         {{Form::text('student_ID', '', ['class' => 'form-control'])}}
-        
-        {{Form::label('netID', 'Last Name')}}
-        {{Form::text('netID', '', ['class' => 'form-control'])}}
-        
-        {{Form::label('course_num', 'Last Name')}}
+    </div>
+    <div class='form_group'>
+        {{Form::label('course_num', 'Course')}}
         {{Form::text('course_num', '', ['class' => 'form-control'])}}
-        
-        {{Form::label('section_num', 'Last Name')}}
+    </div>
+    <div class='form_group'>
+        {{Form::label('section_num', 'Section')}}
         {{Form::text('section_num', '', ['class' => 'form-control'])}}
     </div>
     {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}

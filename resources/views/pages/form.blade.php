74d006bc473e7e10f1544b3fb6201d08edb18697
Nooice88 <56607550+Nooice88@users.noreply.github.com>
2020-10-23 01:16:31 -0500

    update

--- resources/views/pages/form.blade.php
+++ resources/views/pages/form.blade.php
@@ -7,6 +7,21 @@
     <div class='form_group'>
         {{Form::label('first_name', 'First Name')}}
         {{Form::text('first_name', '', ['class' => 'form-control'])}}
+        
+        {{Form::label('last_name', 'Last Name')}}
+        {{Form::text('last_name', '', ['class' => 'form-control'])}}
+
+        {{Form::label('student_ID', 'Last Name')}}
+        {{Form::text('student_ID', '', ['class' => 'form-control'])}}
+        
+        {{Form::label('netID', 'Last Name')}}
+        {{Form::text('netID', '', ['class' => 'form-control'])}}
+        
+        {{Form::label('course_num', 'Last Name')}}
+        {{Form::text('course_num', '', ['class' => 'form-control'])}}
+        
+        {{Form::label('section_num', 'Last Name')}}
+        {{Form::text('section_num', '', ['class' => 'form-control'])}}
     </div>
     {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
     {!! Form::close() !!}
